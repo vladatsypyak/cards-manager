fb2e08e63f73aa4a2fd7008c31f4cb7bca4a0baf
Vladyslava <vladatsyupyak@gmail.com>
2025-07-03 19:31:36 +0200

    feat: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
 	server: {
 		host: true,
 		strictPort: true
