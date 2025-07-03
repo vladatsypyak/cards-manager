7dbd7cacc8ae4d8bd18a15bdb78047d5421440d1
Vladyslava <vladatsyupyak@gmail.com>
2025-07-03 13:55:09 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		host: true,
+		strictPort: true
+	}
+});
